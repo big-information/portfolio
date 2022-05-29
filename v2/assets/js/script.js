57182de549bf228a8e0edfd17871981a5325a577
big-information <davidmarcus@protonmail.com>
2022-05-29 10:44:59 -0500

    updates

--- v2/assets/js/script.js
+++ v2/assets/js/script.js
@@ -1,30 +1,19 @@
 $( "#work" ).click( function() {
-    if ($("#work-categories").hasClass("d-none")) {
-        $("#work-categories").toggleClass("d-none");
-        $("#container-main-0").toggleClass("border-top-left-0");
-        $("#container-main-1").toggleClass("border-bottom-left-0");
-        $("#col-main").toggleClass("box-shadow");
-        $("#col-main").toggleClass("col-md-8");
-        $("#col-main").toggleClass("col-lg-6");
-        $(".row").toggleClass("box-shadow");
-        $("#work").toggleClass("text-white-50");
-        $("#work").toggleClass("text-light");
-        $("#info").toggleClass("text-light");
-        $("#info").toggleClass("text-white-50");
+    if ($("#info").hasClass("text-light")) {
+        swap();
     }
 });
+
 $( "#info" ).click( function() {
-    if (!($("#work-categories").hasClass("d-none"))) {
-        $("#work-categories").toggleClass("d-none");
-        $("#container-main-0").toggleClass("border-top-left-0");
-        $("#container-main-1").toggleClass("border-bottom-left-0");
-        $("#col-main").toggleClass("box-shadow");
-        $("#col-main").toggleClass("col-md-8");
-        $("#col-main").toggleClass("col-lg-6");
-        $(".row").toggleClass("box-shadow");
-        $("#work").toggleClass("text-white-50");
-        $("#work").toggleClass("text-light");
-        $("#info").toggleClass("text-light");
-        $("#info").toggleClass("text-white-50");
-    }  
+    if ($("#work").hasClass("text-light")) {
+        swap();
+    }
 });
+
+function swap() {
+    $("#work").toggleClass("text-light text-white-50");
+    $("#info").toggleClass("text-light text-white-50");
+    $("#col-main").toggleClass(" col-12 col-md-8 col-lg-6");
+    $("#container-main-0").toggleClass("d-none");
+    $("#container-main-1").toggleClass("d-none");
+}
